0dab1bacf3e39ed21f43554ca6da77ff1ae83451
Isaac Toledano <jitoledano98@gmail.com>
2021-04-20 15:11:18 -0600

    First commit

---
+++ src/HomePage/HPStyle.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+    background:#730105;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    height: 100vh;
+
+
+`
+
+export const MainP = styled.p`
+    font-size: 5vh;
+    color: white;
+    text-align: center;
+    font-family: 'Original Surfer', cursive;
+`
---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Main, MainP} from './HPStyle';
+const HomePage = () => {
+    return (
+            <Main>
+                <MainP>Welcome, Dungeon Masters, to the last website you'll ever need.</MainP>
+            </Main>
+    )
+}
+
+export default HomePage
